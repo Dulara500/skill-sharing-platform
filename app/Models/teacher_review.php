404c2d2b6a3f73c3c882ea400c5c32b12fe46c9f
Dulara500 <madushankadulara38@gmail.com>
2026-02-01 23:31:49 +0530

    teacher_review model

---
+++ app/Models/teacher_review.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class teacher_review extends Model
+{
+    protected $fillable = [
+        'Teacher_id',
+        'Student_id',
+        'course_title',
+        'review',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'Teacher_id');
+    }
+}
