0796f6d87742d5cb722ab5820419fb590a295c74
Dulara500 <madushankadulara38@gmail.com>
2026-02-01 23:33:49 +0530

    new studentEvaluation user view

---
+++ resources/views/user/studentEvaluation.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/usernavbarcss.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+    <style>
+        a {
+            text-decoration: none;
+            color: inherit;
+        }
+    </style>
+</head>
+<body>
+    @include('user.usernavbar')
+    @include('user.calender')
+    <div class="container mt-5" style="padding-top: 120px;">
+        <div class="row justify-content-center">
+
+            <div class="col-lg-8 mb-5">
+                <div class="card p-4">
+                    <h2>Student Evaluation</h2>
+                    <hr>
+
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</body>
+</html>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
