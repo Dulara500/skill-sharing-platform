e38fa1228a3c7e7dec063e35c93148720952e267
Dulara500 <madushankadulara38@gmail.com>
2026-02-03 17:44:35 +0530

    new view to evaluate students

--- resources/views/user/studentEvaluation.blade.php
+++ resources/views/user/studentEvaluation.blade.php
@@ -23,9 +23,28 @@
 
             <div class="col-lg-8 mb-5">
                 <div class="card p-4">
+
                     <h2>Student Evaluation</h2>
                     <hr>
-
+                    <span>student name:{{ $studentname->user->name }}</span>
+                    <span>Course taken: {{ $stucourse->first()->course_title }}</span>
+                    <form method="POST" action="{{ route('teareview.store') }}">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="review" class="form-label">Evaluation Comments</label>
+                            <textarea class="form-control" id="review" name="review" rows="4" placeholder="Enter your evaluation comments here..."></textarea>
+                        </div>
+                        <input type="hidden" name='name' value='{{ $studentname->user->name }}'>
+                        <input type="hidden" name= 'course_title' value='{{ $stucourse->first()->course_title }}'>
+                        <input type="hidden" name='user_id' value='{{ $studentname->user->id }}'>
+                        <input type="hidden" name='teacher_id' value='{{ Auth::id() }}'>
+                        <button type="submit" class="btn btn-primary">Submit Evaluation</button>
+                    </form>
+                    @if(@session('success'))
+                        <div class="alert alert-success mt-3">
+                            {{ session('success') }}
+                        </div>
+                    @endif
                 </div>
             </div>
 
